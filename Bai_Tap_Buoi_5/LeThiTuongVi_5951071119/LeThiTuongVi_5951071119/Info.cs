a38c34a046114d594dfc7b8e5406b810668ba50c
LeThiTuongVi2612 <75873350+LeThiTuongVi2612@users.noreply.github.com>
2021-03-22 21:08:24 +0700

    LeThiTuongVi_5951071119 da dang bai tap

---
+++ Bai_Tap_Buoi_5/LeThiTuongVi_5951071119/LeThiTuongVi_5951071119/Info.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeThiTuongVi_5951071119
+{
+    public class Info
+    {
+        private String thoiGian;
+        private String noiDung;
+        private String link;
+
+        public string ThoiGian { get => thoiGian; set => thoiGian = value; }
+        public string NoiDung { get => noiDung; set => noiDung = value; }
+        public string Link { get => link; set => link = value; }
+    }
+}
---
+++ Bai_Tap_Buoi_5/LeThiTuongVi_5951071119/LeThiTuongVi_5951071119/WebFace.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFace.aspx.cs" Inherits="LeThiTuongVi_5951071119.WebFace" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="value" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
